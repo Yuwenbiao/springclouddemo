4d3addb2baaca3fa0c236dfa10a391e98224797a
余文彪 <yuwb@corp.21cn.com>
2020-02-10 17:08:21 +0800

    - zuul网关请求过滤 - 新增http测试文件

--- zuuldemo/src/main/java/com/example/zuuldemo/ZuulDemoApplication.java
+++ zuuldemo/src/main/java/com/example/zuuldemo/ZuulDemoApplication.java
@@ -1,8 +1,10 @@
 package com.example.zuuldemo;
 
+import com.example.zuuldemo.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
@@ -12,4 +14,8 @@ public class ZuulDemoApplication {
         SpringApplication.run(ZuulDemoApplication.class, args);
     }
 
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
 }
